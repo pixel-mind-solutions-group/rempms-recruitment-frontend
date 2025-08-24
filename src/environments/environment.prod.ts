215bc0ad08ee610699fd9118bb18da19958f2ae9
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2025-08-24 06:49:53 +0530

    changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+
+  baseUrl: 'http://localhost:8081/api',
+  loginUrl: 'http://localhost:9001/api/iam',
+
+  uuid: 'e7a75653-1d2c-4fbd-a0f7-2f7b7b9bb2f9',
+};
